bb5f9dcdb9d268f6bc0c2ca83c291273508d452f
kei2100 <kei.arima.2100@gmail.com>
2019-05-20 20:05:09 +0900

    Add test

--- file_test.go
+++ file_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestOpenFile(t *testing.T) {
+func TestRenameFileWhileOpening(t *testing.T) {
 	dir, err := ioutil.TempDir("", "fsdtest")
 	if err != nil {
 		panic(err)
@@ -42,6 +42,49 @@ func TestOpenFile(t *testing.T) {
 	}
 	if g, w := string(b), "foobar"; g != w {
 		t.Errorf("content got %v, want %v", g, w)
+	}
+}
+
+func TestDeleteFileWhileOpening(t *testing.T) {
+	dir, err := ioutil.TempDir("", "fsdtest")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	of, err := openFileForTest(filepath.Join(dir, "foo"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	f := &onceCloseFile{File: of}
+	defer f.Close()
+
+	if _, err := f.WriteString("foo"); err != nil {
+		t.Fatalf("failed to write foo: %+v", err)
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		t.Fatalf("failed to remove foo: %+v", err)
+	}
+	if _, err := f.WriteString("bar"); err != nil {
+		t.Fatalf("faield to write bar: %+v", err)
+	}
+
+	if _, err := f.Seek(0, 0); err != nil {
+		t.Fatalf("failed to seek: %+v", err)
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatalf("failed to read: %+v", err)
+	}
+	if g, w := string(b), "foobar"; g != w {
+		t.Errorf("content got %v, want %v", g, w)
+	}
+
+	if err := f.Close(); err != nil {
+		t.Fatalf("faield to close: %+v", err)
+	}
+	if fi, err := os.Stat(f.Name()); err == nil || !os.IsNotExist(err) {
+		t.Fatalf("unexpected file stat fi=%+v err=%+v", fi, err)
 	}
 }
 
